5c82a03a7076989fdbef38248ffc04169d34a960
Will Ellwood <fragmad@gmail.com>
2017-06-04 17:36:08 +0100

    XML Beers
    This commit puts XML data into a postgres database.
    
    This isn't super clean and needs some error correcting and conditional
    checking, but that can be an exercise for tomorrow or something.
    
    After that the next steps are:
      - Moar DATA
      - Indexing
      - Measuring query times

--- app/benchmark-postgresql-xml.py
+++ app/benchmark-postgresql-xml.py
@@ -1,6 +1,12 @@
 from string import Template
 import psycopg2
 
+beers = [("The Curse of Threepwood", "Silchester (Near Reading), United Kingdom"),
+         ("Vixen", "Loughborough, United Kingdom"),
+         ("West End Stout", "Leicester, United Kingdom"),
+         ("Guiness", "Dublin, Ireland"),
+         ("Cloudwater Bergamot Sour", "Manchester, United Kingdom")]
+
 
 class DBStuff(object):
     def __init__(self, dbname, user, password, host, port):
@@ -19,6 +25,9 @@ class DBStuff(object):
 
     def fetchone(self):
         return self.cur.fetchone()
+
+    def fetchall(self):
+        return self.cur.fetchall()
     
     def commit(self):
         self.conn.commit()
@@ -39,17 +48,32 @@ def main1():
 
 class XMLObject(object):
     def __init__(self):
-        self.template_string = """ 
-         <beer><name>$name</name><location>$location</location></beer>
-        """
+        self.template_string = """<beer><name>$name</name><location>$location</location></beer>"""
         self.xml_template = Template(self.template_string)
 
     def stamp_xml(self, name, location):
         return self.xml_template.substitute(name=name, location=location)
 
 def main2():
-    beers = XMLObject()
-    print beers.stamp_xml("The Curse of Threepwood", "Silchester (Near Reading), United Kingdom")
-    
+    beer = XMLObject()
+    print beer.stamp_xml("The Curse of Threepwood", "Silchester (Near Reading), United Kingdom")
+
+def main3():
+    beer_xml = XMLObject()
+    db = DBStuff("postgres", "postgres", "postgres", "db", "5432")
+    db.connect()
+    db.query("CREATE TABLE beers (id serial PRIMARY KEY,  beer_xml xml, beer_name varchar)")
+    db.query("INSERT INTO beers (beer_xml, beer_name) VALUES ('{}', '{}')".format(beer_xml.stamp_xml(beers[0][0], beers[0][1]), beers[0][0]))
+    db.query("INSERT INTO beers (beer_xml, beer_name) VALUES ('{}', '{}')".format(beer_xml.stamp_xml(beers[1][0], beers[1][1]), beers[1][0]))
+    db.query("INSERT INTO beers (beer_xml, beer_name) VALUES ('{}', '{}')".format(beer_xml.stamp_xml(beers[2][0], beers[2][1]), beers[2][0]))
+    db.query("INSERT INTO beers (beer_xml, beer_name) VALUES ('{}', '{}')".format(beer_xml.stamp_xml(beers[3][0], beers[3][1]), beers[3][0]))
+    db.query("INSERT INTO beers (beer_xml, beer_name) VALUES ('{}', '{}')".format(beer_xml.stamp_xml(beers[4][0], beers[4][1]), beers[4][0]))
+    db.query("SELECT * from beers")
+    beer_list = db.fetchall()
+    db.commit()
+    db.close()
+
+    for beer in beer_list:
+        print beer[0], beer[1], beer[2] 
 if __name__ == '__main__':
-   main2() 
+   main3() 
